7776b044c75533840250fdd7f921c19c2dd4c5f6
Sylvain Roy <sylvain.roy.81@gmail.com>
2014-11-22 12:40:37 -0500

    Refactor list view to handle new item POSTs

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -15,7 +15,7 @@ class ItemValidationTest(FunctionalTest):
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # She tries again with some text for the item, which now works
-        self.browser.fin_element_by_id('id_new_item').send_keys('Buy milk\n')
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
         self.check_for_row_in_list_table('1: Buy milk')
 
         # Perversely, she now decides to submit a second blank list item
