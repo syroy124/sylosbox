779e91df5c2d624a6996bee97fc5fb9959eda862
Sylvain Roy <sylvain.roy.81@gmail.com>
2014-11-23 19:46:17 -0500

    Add a new test to make sure error disapear on click

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -61,3 +61,17 @@ class ItemValidationTest(FunctionalTest):
 
     def get_error_element(self):
         return self.browser.find_element_by_css_selector('.has-error')
+
+    def test_error_message_are_cleared_on_click(self):
+        # Edith starts a new list in a way that causes a validation error:
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('\n')
+        error = self.get_error_element()
+        self.assertTrue(error.is_displayed())
+
+        # She click in the input box
+        self.get_item_input_box().click()
+
+        # She is pleased to see that the error message disappears
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())
