ce61a7cfe7eb096d4476336ef585543916ce57e8
Sylvain Roy <sylvain.roy.81@gmail.com>
2014-12-09 18:22:14 -0500

    add list owners via forms. more isolated tests

--- lists/forms.py
+++ lists/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from lists.models import Item
+from lists.models import Item, List
 from django.core.exceptions import ValidationError
 
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
@@ -7,10 +7,6 @@ DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 
 class ItemForm(forms.models.ModelForm):
     
-    def save(self, for_list):
-        self.instance.list = for_list
-        return super().save()
-
     class Meta:
         model = Item
         fields = ('text',)
@@ -24,6 +20,14 @@ class ItemForm(forms.models.ModelForm):
             'text': {'required': EMPTY_ITEM_ERROR}
         }
 
+class NewListForm(ItemForm):
+    
+    def save(self,owner):
+        if owner.is_authenticated():
+            return List.create_new(first_item_text=self.cleaned_data['text'], owner=owner)
+        else:
+            return List.create_new(first_item_text=self.cleaned_data['text'])
+
 class ExistingListItemForm(ItemForm):
     def __init__(self, for_list, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -36,6 +40,3 @@ class ExistingListItemForm(ItemForm):
             e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
             self._update_errors(e)
 
-    def save(self):
-        return forms.models.ModelForm.save(self)
-    
--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 from django.core.exceptions import ValidationError
 
-from lists.forms import ExistingListItemForm, ItemForm
+from lists.forms import ExistingListItemForm, ItemForm, NewListForm
 from lists.models import Item, List
 
 from django.contrib.auth import get_user_model
@@ -22,12 +22,9 @@ def view_list(request, list_id):
     return render(request, 'list.html', {'list': list_, "form": form})
 
 def new_list(request):
-    form = ItemForm(data=request.POST)
+    form = NewListForm(data=request.POST)
     if form.is_valid():
-        list_ = List()
-        list_.owner = request.user
-        list_.save()
-        form.save(for_list=list_)
+        list_ = form.save(owner=request.user)
         return redirect(list_)
     else:
         return render(request, 'home.html', {"form": form})
